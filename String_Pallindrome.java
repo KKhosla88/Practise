61f4e7acc5b82c17de10c1b3b43b0cfa7ebd27b1
KKhosla88 <53813006+KKhosla88@users.noreply.github.com>
2020-05-05 23:05:27 +0530

    Add files via upload

---
+++ String_Pallindrome.java
@@ -0,0 +1,25 @@
+package java_Interview_Ques;
+import java.util.*;
+
+public class String_Pallindrome {
+
+	public static void main(String[] args) 
+	{
+		String original,reverse="";
+		Scanner s=new Scanner(System.in);
+		System.out.print("Enter String: ");
+		original=s.nextLine();
+		int len=original.length();
+		for(int i=len-1;i>=0;i--)
+		{
+			reverse+=original.charAt(i);
+		}
+		if(original.equals(reverse))
+		{
+			System.out.println("Stirng is pallindrome");
+		}
+		else
+			System.out.println("String is not pallindrome");
+	}
+
+}
