c6c22fd1aa03ac424821226b09bc146407f8fcb3
KKhosla88 <53813006+KKhosla88@users.noreply.github.com>
2020-05-09 23:13:12 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,18 @@
+package java_Interview_Ques;
+import java.util.Scanner;
+public class Factorial {
+
+	public static void main(String[] args) 
+	{
+	  Scanner s=new Scanner(System.in);
+	  int n=s.nextInt();
+	  int fact=1;
+	  for(int i=1;i<=n;i++)
+		  {
+			  fact=fact*i;
+		  }
+	  
+	  System.out.print("Factorial of"+" "+n+":"+fact);
+	}
+
+}
