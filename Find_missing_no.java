2a3542fed6b7f1b2efa8f8e5f8e7c0cb51ed9373
KKhosla88 <53813006+KKhosla88@users.noreply.github.com>
2020-05-15 19:45:48 +0530

    Update Find_missing_no.java

--- Find_missing_no.java
+++ Find_missing_no.java
@@ -5,15 +5,8 @@ public class Find_missing_no {
 	public static void main(String[] args) 
 	{
 		int arr[]= {1,2,4,7,6,5};
-//	    Scanner s=new Scanner(System.in);
-//		int n=s.nextInt();
-//		int arr[]=new int[n];
 		int xor1=arr[0];
 		int xor2=1;
-//		for(int i=0;i<=n;i++)
-//		{
-//			arr[i]=s.nextInt();
-//		}
 		for(int i=0;i<arr.length;i++)
 		{
 			xor1=xor1^arr[i];
