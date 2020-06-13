dedc6c3b8896b644cff7ab5c182c42324ad973c1
KKhosla88 <53813006+KKhosla88@users.noreply.github.com>
2020-06-13 23:08:15 +0530

    Update Leap_year.java

--- Leap_year.java
+++ Leap_year.java
@@ -7,7 +7,7 @@ public class Leap_year {
 		System.out.print("Enter Year: ");
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt();
-		if((n%400==0) || (n%4==0 && n%100!=0))
+		if((n%400==0) || (n%4==0 && n%100!=0))           // year is leap year if it is eveny didvided by both 100 & 400
 		{ 
 			if(n>2020)
 			System.out.println("It is leap year");
